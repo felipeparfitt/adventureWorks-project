0915792a6fb04e3b5a5539d5fb5909a7c30bdd84
felipeparfitt@gmail.com <felipeparfitt@gmail.com>
2024-10-24 19:03:05 +0000

    fixing code in project setup notebook

--- azure_databricks/utils/project_setup.py
+++ azure_databricks/utils/project_setup.py
@@ -8,9 +8,6 @@
 # MAGIC
 # MAGIC - **Create Schemas with Managed Storage:**
 # MAGIC Sets up schemas within a catalog, organizing data into different layers (such as bronze, silver, or gold) and assigning a specific storage location for each schema.
-# MAGIC
-# MAGIC - **Create Tables in Schemas:**
-# MAGIC Handles the creation of tables within a specified schema and catalog by executing predefined SQL commands, ensuring proper placement and management of the tables.
 
 # COMMAND ----------
 
@@ -41,94 +38,10 @@ def create_schema(catalog_name, layer, path):
               """)
     print("************************************")
 
-# def creating_tables(environment, layer, table_dict):
-#     catalog_name = f"dbproj_{environment}"
-#     print(f"Using {catalog_name} catalog")
-#     spark.sql(f"USE CATALOG {catalog_name};")
-
-#     print(f"Using {layer} schema")
-#     spark.sql(f"USE SCHEMA {layer};")
-
-#     for table_name, create_table_sql in table_dict.items():
-#         print(f"Creating {table_name} table in {catalog_name}.{layer}: ", end='')
-#         spark.sql(create_table_sql)
-#         print("Success !!")
-#         print("************************************")
-
-
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC ### Creating Catalog, Schemas and Tables
-
-# COMMAND ----------
-
-# Dict used to create the tables on bronze layer
-sql_tables = {
-    "clients": """
-        CREATE TABLE IF NOT EXISTS clients (
-            client_id INT,
-            firstname VARCHAR(30),
-            lastname VARCHAR(30),
-            birth_date DATE,
-            email STRING,
-            phone CHAR(20),
-            extract_Time TIMESTAMP
-        );
-    """,
-    "client_addresses": """
-        CREATE TABLE IF NOT EXISTS client_addresses (
-            address_id INT,
-            client_id INT,
-            state CHAR(2),
-            city VARCHAR(100),
-            street VARCHAR(150),
-            zip_code VARCHAR(20),
-            extract_Time TIMESTAMP
-        );
-    """,
-    "sales_people": """
-        CREATE TABLE IF NOT EXISTS sales_people (
-            salesperson_id INT,
-            firstname VARCHAR(30),
-            lastname VARCHAR(30),
-            email VARCHAR(100),
-            phone_number VARCHAR(20),
-            extract_Time TIMESTAMP
-        );
-    """,
-    "sales": """
-        CREATE TABLE IF NOT EXISTS sales (
-            sale_id INT,
-            client_id INT,
-            salesperson_id INT,
-            sale_date DATE,
-            total_amount DECIMAL(10, 2),
-            extract_Time TIMESTAMP
-        );
-    """,
-    "sales_items": """
-        CREATE TABLE IF NOT EXISTS sales_items (
-            item_id INT,
-            sale_id INT,
-            product_id INT,
-            quantity INT,
-            unit_price DECIMAL(10, 2),
-            total_amount DECIMAL(10, 2),
-            discount DECIMAL(10, 2),
-            extract_Time TIMESTAMP
-        );
-    """,
-    "products": """
-        CREATE TABLE IF NOT EXISTS products (
-            product_id INT,
-            product_name VARCHAR(150),
-            description STRING,
-            price DECIMAL(10, 2),
-            extract_Time TIMESTAMP
-        );
-    """
-}
+# MAGIC ### Creating Catalog and all schemas
 
 # COMMAND ----------
 
@@ -139,6 +52,3 @@ create_catalog(catalog_name=catalog_name)
 create_schema(catalog_name=catalog_name, layer='bronze', path=bronze_path)
 create_schema(catalog_name=catalog_name, layer='silver', path=silver_path)
 create_schema(catalog_name=catalog_name, layer='gold', path=gold_path)
-
-# Creating all tables needed
-#creating_tables(env, 'bronze', sql_tables)
